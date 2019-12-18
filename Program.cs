2639494751e789ef52de377e9d1167063bd7eec6
Alan Yang <alan199402@gmail.com>
2019-12-18 10:18:03 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace WcfChatRoom
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
+        // The main entry point for the application.
+        
         [STAThread]
         static void Main()
         {
