f1f43cb753042d50622d328d6fa63d288b2691a5
Alan Yang <alan199402@gmail.com>
2019-12-18 10:20:46 -0500

    Update VeryBasicCommunicationService.cs

--- VeryBasicCommunicationService.cs
+++ VeryBasicCommunicationService.cs
@@ -4,29 +4,24 @@
 
     public delegate void MessagePostHandler(string message);
 
-    /// <summary>
-    /// Interface for a very basic WCF service
-    /// </summary>
+
+    
     [ServiceContract]
     public interface IVeryBasicCommunicationService
     {
-        /// <summary>
-        /// A basic method that is used for transmition/receiving of messages
-        /// </summary>
-        /// <param name="message">The message</param>
+        
+
         [OperationContract]
         void Transmit(string message);
     }
 
-    /// <summary>
-    /// A very basic WCF Service example
-    /// </summary>
+
+  
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class VeryBasicCommunicationService : IVeryBasicCommunicationService
     {
-        /// <summary>
-        /// Post Message Delegate Method
-        /// </summary>
+        
+      
         private MessagePostHandler postMessageMethod { get; set; }
 
         public VeryBasicCommunicationService(MessagePostHandler callbackMethod)
@@ -34,10 +29,7 @@
             this.postMessageMethod = callbackMethod;
         }
 
-        /// <summary>
-        /// A basic method that is used for transmition/receiving of messages
-        /// </summary>
-        /// <param name="message">The message</param>
+
         public void Transmit(string message)
         {
             if (this.postMessageMethod != null)
